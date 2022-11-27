2570a62e5599c15bab75ea8648a933b22044afb8
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 19:00:51 +0200

    Add project files.

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab1ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine descrescatoare.
+                • Exemplu: citim 3,9,0 Afisam : 9 3 0
+             */
+
+            Console.WriteLine("Acest program va afisa 3 numere in ordine descrescatoare");
+
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al treilea numar");
+            int z = int.Parse(Console.ReadLine());
+
+            int[] nums = { x, y, z };
+
+            Array.Sort(nums);
+            Array.Reverse(nums);
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
